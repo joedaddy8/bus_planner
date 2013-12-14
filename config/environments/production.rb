679a007dfd91b464e993530f57828eea3b052041
Dan <danbarring@gmail.com>
2013-12-14 17:36:10 -0500

    fdas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,11 @@ BusApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+  # Generate digests for assets URLs
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
